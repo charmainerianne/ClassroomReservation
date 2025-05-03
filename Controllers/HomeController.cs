16cbf8b6f0946d0971c60e4b0762783234270e6c
Charmaine Rianne Q. Bordario <158406413+charmainerianne@users.noreply.github.com>
2025-05-03 15:20:49 +0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using CTUClassroomReservation.Models;
+
+namespace YourProjectName.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Dashboard");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
